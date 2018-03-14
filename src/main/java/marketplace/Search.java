3c7f1d74b7654e89b748ec68402996fc1913b868
Mark Mazur <markmazur94@gmail.com>
2018-03-14 16:46:45 -0500

    Updated the index page with a search form to allow for searches

---
+++ src/main/java/marketplace/Search.java
@@ -0,0 +1,22 @@
+package marketplace;
+
+public class Search {
+    private String keywords;
+    private String city;
+
+    public String getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
--- src/main/java/marketplace/WebController.java
+++ src/main/java/marketplace/WebController.java
@@ -3,15 +3,51 @@ package marketplace;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WebController {
 
+    /*
     @GetMapping("/hello")
     public String hello(@RequestParam(name="name", required=false, defaultValue="Heroku") String name, Model model) {
         model.addAttribute("name", name);
         return "index";
+    }
+    */
+
+    /*
+    @GetMapping("/index")
+    public String searchForm(Model model) {
+        model.addAttribute("search", new Search());
+        return "index";
+    }
+
+    @PostMapping("/search")
+    public String searchSubmit(@ModelAttribute Search search) {
+        return "result";
+    }
+    */
+
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/search";
+    }
+
+
+    @GetMapping("/search")
+    public String searchForm(Model model) {
+        model.addAttribute("search", new Search());
+        return "search";
+    }
+
+    @PostMapping("/search")
+    public String searchSubmit(@ModelAttribute Search search) {
+        return "result";
     }
 
 }
